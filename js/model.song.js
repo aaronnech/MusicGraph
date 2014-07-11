4bfac31cb8a5cc559e0d0f0ee878574ee60b8a86
Aaron Nech <aaronnech@gmail.com>
2014-07-11 01:40:04 -0700

    fixed

--- js/model.song.js
+++ js/model.song.js
@@ -1,48 +1,24 @@
-function Song(api_obj, songId) {
+function Song(track) {
 	var self = this;
 
-	/* Instantiate the global sp object; include models & views */
-	var sp = getSpotifyApi(1);
-	var models = sp.require("sp://import/scripts/api/models");
-	var views = sp.require("sp://import/scripts/api/views");
-	 
-	/* Create an array of tracks from the user's library */
-	var track = models.Track.fromURI('spotify:track:7DJI0fg4dzBJk31sEmn6Zk');
-	 
-	/* Create a temporary playlist for the song */
-	var playlist = new models.Playlist();
-	playlist.add(track);
-	var playerView = new views.Player();
-	playerView.track = null; // Don't play the track right away
-	playerView.context = playlist;
-	 
-	/* Pass the player HTML code to the #player <div /> */
-	$('#player').append(playerView.node);
-
-	var fetchArtist = function(artistId, callback) {
-		$.ajax({
-			url: 'https://api.spotify.com/v1/artists/' + artistId,
-			success: function(response) {
-				callback(response);
-			}
-		});
+	var startSound = function(id, url, ended, loop) {
+		soundHandle = document.getElementById(id);
+		var newSoundHandle = old_element.cloneNode(true);
+		soundHandle.parentNode.replaceChild(newSoundHandle, soundHandle);
+		soundHandle = newSoundHandle;
+		soundHandle.setAttribute('src', url);
+		if(loop)
+			soundHandle.setAttribute('loop', loop);
+		soundHandle.play();
+		soundHandle.addEventListener('ended', ended, false);
 	};
-	
-	var fetchRelatedSongs = function(artistId, callback) {
-		api.getSong(name)
-		$.ajax({
-			url: 'https://api.spotify.com/v1/artists/' + artistId + '/related-artists',
-			success: function(response) {
-				callback(response);
-			}
-		});
+
+	var PLAYER_ID = 'audio-player';
+
+	self.name = track.name;
+	self.url = track.preview_url;
+
+	self.play = function(ended) {
+		startSound(PLAYER_ID, self.url, ended, false);
 	}
-
-	var artist = fetchArtist()
-	//list of related songs
-	var relatedSongs = fetchRelatedSongs(artistId, callback)
-	//list of youtube related videos 
-
-
-
 }
