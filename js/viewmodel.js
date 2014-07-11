815353600fb4236951577adb16cdc12c4d2a4892
Aaron Nech <aaronnech@gmail.com>
2014-07-11 03:50:49 -0700

    fixed

--- js/viewmodel.js
+++ js/viewmodel.js
@@ -6,8 +6,29 @@ function AppViewModel() {
 	self.nodes = new ko.observableArray([]);
 	self.edges = new ko.observableArray([]);
 
+	var makeArtists = function(node, artists) {
+		self.edges.removeAll();
+		self.nodes.removeAll();
+		var nodes = artists.map(function(artist) {
+			return new Node(self, artist, NODE_TYPES.ARTIST);
+		});
+		self.addNode(node);
+		nodes.forEach(function(artistNode) {
+			self.addEdge(new Edge(node, artistNode));
+			self.addNode(artistNode);
+		});
+	};
+
+	var makeSongs = function(songs) {
+
+	};
+
 	self.addNode = function(node) {
 		self.nodes.push(node);
+	};
+
+	self.addEdge = function(edge) {
+		self.edges.push(edge);
 	};
 
 	self.removeNode = function(node) {
@@ -18,7 +39,7 @@ function AppViewModel() {
 	};
 
 	self.clickNode = function(node) {
-		node.onClick(self.expandPlayer);
+		node.onClick(self.explore);
 	};
 
 	self.nodeMouseOver = function(node) {
@@ -29,7 +50,21 @@ function AppViewModel() {
 
 	};
 
-	self.expandPlayer = function(node, result) {
-		
+	self.playSong = function(song) {
+		song.play();
+	};
+
+	self.explore = function(node, result) {
+        switch (self.nodeType) {
+            case NODE_TYPES.GENRE:
+                makeArtists(node, result);
+                break;
+            case NODE_TYPES.ARTIST:
+                makeSongs(node, result);
+                break;
+            case NODE_TYPES.SONG:
+                self.playSong(result);
+                break;
+        }
 	};
 }
