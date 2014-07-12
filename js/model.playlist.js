ad59fdd72002953688c89307ed3d5858109d52ba
Ryan Drapeau <drapeau@cs.washington.edu>
2014-07-12 07:33:52 -0700

    Track list

--- js/model.playlist.js
+++ js/model.playlist.js
@@ -17,8 +17,11 @@ function Playlist() {
     };
 
     self.getTracks = function() {
-
-        //TODO
+        var results = [];
+        for (var i = 0; i < self.songs().length; i++) {
+            results.push(self.songs()[i].track);
+        }
+        return results;
     };
 
     // ADD A SONG TO THE END OF THE PLAYLIST
