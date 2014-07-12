eec65945962a682ffd255a6755037fc7910681a9
Ryan Drapeau <drapeau@cs.washington.edu>
2014-07-11 21:02:07 -0700

    Fixes

--- js/model.playlist.js
+++ js/model.playlist.js
@@ -6,20 +6,20 @@ function Playlist(vm) {
 
     // ADD A SONG TO THE END OF THE PLAYLIST
     self.addSong = function(song) {
-        self.songs().push(song);
+        self.songs.push(song);
     };
 
     // REMOVE A SONG FROM THE PLAYLIST
     self.removeSong = function(song) {
-        var index = self.songs().indexOf(song);
+        var index = self.songs.indexOf(song);
         if (index > -1) {
-            self.songs().splice(index, 1);
+            self.songs.splice(index, 1);
         }
     };
 
     // RETURNS THE LENGTH OF THE PLAYLIST
     self.length = function() {
-        return self.songs().length;
+        return self.songs.length;
     };
 
     // HANDLES CLICK EVENTS ON SONGS IN THE PLAYLIST
