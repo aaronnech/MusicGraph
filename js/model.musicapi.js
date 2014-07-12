c4cffe46856ff1f13ec0805a3899805ea9e39960
Setareh Lotfi <slotfi@andrew.cmu.edu>
2014-07-11 22:55:22 -0700

    Adding Genre

--- js/model.musicapi.js
+++ js/model.musicapi.js
@@ -1,3 +1,33 @@
+//Load Genere and top Artist relate to that
+jQuery.ajaxSettings.traditional = true; 
+var host = 'http://developer.echonest.com/api/v4/';
+var apiKey =  'YTBBANYZHICTAFW2P';
+var deferredSongs = null;
+var allGenres = {};
+var genreList = ['rock'];
+var fullGenreList = [];
+var player;
+
+
+
+function loadTopArtists(genreName) {
+    var url = host + 'genre/artists'
+    $.getJSON(url, {api_key:apiKey, name:genreName },
+        function(data) {
+            var artists = data.response.artists;
+            var list = $("#artist-list");
+            list.empty();
+            _.each(artists, function(artist, i) {
+                var a = $("<a>").text(artist.name).attr('href', 'http://static.echonest.com/echotron/?id='+ artist.id);
+                var li = $("<li>").append(a);
+                list.append(li);
+            });
+        });
+}
+
+
+
+
 // find template and compile it
 var templateSource = document.getElementById('results-template').innerHTML,
     template = Handlebars.compile(templateSource),
