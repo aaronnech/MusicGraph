ac7b45c5e56917ba2e22f19f1aab06598cf3964f
Ryan Drapeau <drapeau@cs.washington.edu>
2014-07-12 01:27:58 -0700

    fixes

--- js/model.musicapi.js
+++ js/model.musicapi.js
@@ -1,71 +1,27 @@
-jQuery.ajaxSettings.traditional = true;
-var host = 'http://developer.echonest.com/api/v4/';
-var apiKey = 'YTBBANYZHICTAFW2P';
-var knownSongs = {};
-var curGenre = null;
-var curSong = null;
-var allGenres = {};
-var genreList = ['rock'];
-var fullGenreList = [];
-var player;
-
-function showGenre(genreName) {
-    console.log('showing genre', genreName);
-    curGenre = genreName;
-    var title = genreName;
-    selectGenre(curGenre);
-    loadSimilarGenres(genreName);
-    loadTopArtists(genreName);
-    loadTopSongs(genreName, 'core-best');
-}
-
-// function loadGenre() {
-//     $.getJSON({api_key:apiKey, results:2000, bucket:[]}, function(data) {
-//                 genreList = data.response.genres;
-//             $.each(data.response.genres, function(genre, i) {
-//             genre.which = i;
-//             allGenres[genre.name] = genre;
-//             allGenres[genre.name] = genre;
-//            fullGenreList.push(genre);
-//            console.log(genre);
-//     });
-// });
-// }
-
-function selectGenre(name) {
-    $('#genre').val(name);
-    $('#genre').trigger("liszt:updated");
-}
-
-
-function loadTopArtists(genreName) {
-    var url = host + 'genre/artists'
-
+function loadTopArtists(genreName, callback) {
+    var url = 'http://developer.echonest.com/api/v4/genre/artists'
+    var apiKey = 'YTBBANYZHICTAFW2P';
     $.getJSON(url, {api_key:apiKey,results:10,name:genreName},
         function(data) {
-            var artists = data.response.artists;
-            console.log(artists);
-            // $.each(artists, function(artist, i) {
-            //     var a = 'http://static.echonest.com/echotron/?id='+ artist.id;
-            //     console.log(artist.id);
-
-            // });
+            var results = [];
+            for (var i = 0; i < data.response.artists.length; i++) {
+                results.push(new Artist(data.response.artists[i]));
+            }
+            callback(results);
         });
-}
+};
 
-function loadSimilarGenres(genreName) {
-    var url = host + 'genre/similar'
-    $.getJSON(url, {api_key:apiKey, name:genreName },
-        function(data) {
-            var genres = data.response.genres;
-        });
-    $.each(genres, function(genre, i) {
-                    var a = $("<a>").text(genre.name).attr('href', '?genre='+genre.name);
-                    console.log(a);
-});
-}
-
-
+// function loadSimilarGenres(genreName) {
+//     var url = host + 'genre/similar'
+//     $.getJSON(url, {api_key:apiKey, name:genreName },
+//         function(data) {
+//             var genres = data.response.genres;
+//         });
+//     $.each(genres, function(genre, i) {
+//                     var a = $("<a>").text(genre.name).attr('href', '?genre='+genre.name);
+//                     console.log(a);
+//         });
+// }
 
 //// Spotify API ////
 
